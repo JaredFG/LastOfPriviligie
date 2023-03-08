3e086a3a2d88a9bf32ca15598dec35f42fd5bc35
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2023-03-08 15:48:07 -0600

    updates

---
+++ LastOfPriviligie/Assets/Scripts/LayerSorter.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSorter : MonoBehaviour
+{
+    [SerializeField]
+    private int sortingOrderBase = 5000;
+    [SerializeField]
+    private int offset = 0;
+    [SerializeField]
+    private bool runOnlyOnce = false;
+    private float timer;
+    private float timerMax = .1f;
+    private Renderer myRenderer;
+
+    void Awake()
+    {
+        myRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            timer = timerMax;
+            myRenderer.sortingOrder = (int)(sortingOrderBase - transform.position.y - offset);
+            if (runOnlyOnce)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
